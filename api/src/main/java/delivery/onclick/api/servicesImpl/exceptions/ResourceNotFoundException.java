9d5285f3696603e04c4dc7536fb8233cdf04a33d
guto <gutob84@gmail.com>
2023-07-21 21:24:03 -0300

    feat: ResourceNotFoundException message update

--- api/src/main/java/delivery/onclick/api/servicesImpl/exceptions/ResourceNotFoundException.java
+++ api/src/main/java/delivery/onclick/api/servicesImpl/exceptions/ResourceNotFoundException.java
@@ -2,7 +2,7 @@ package delivery.onclick.api.servicesImpl.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
 
-	public ResourceNotFoundException(Object id) {
-		super("Resource not found, Id " + id);
+	public ResourceNotFoundException() {
+		super("Resource not found");
 	}
 }
